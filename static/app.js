7b82d76ed76117a261fd672b2f4a633d1ad569e0
Mihai Bivol <mm.bivol@gmail.com>
2016-05-19 18:45:28 +0200

    Nince nav options

--- static/app.js
+++ static/app.js
@@ -1,13 +1,40 @@
 var app = angular.module('diffApp', ['ds.objectDiff', 'ngSanitize']);
 
 app.directive('mergeView', function() {
-    var controller = function($scope, ObjectDiff) {
+    var controller = function($scope, $location, $anchorScroll, ObjectDiff) {
         $scope.rootHeadDiff = ObjectDiff.diff($scope.mergeInfo.rhRoot,
                                               $scope.mergeInfo.rhHead)
         $scope.rootUpdateDiff = ObjectDiff.diff($scope.mergeInfo.ruRoot,
                                                 $scope.mergeInfo.ruUpdate)
         $scope.headMergedDiff = ObjectDiff.diff($scope.mergeInfo.hmHead,
                                                 $scope.mergeInfo.hmMerged)
+        $scope.scrollTo = function(id) {
+            if ($location.hash() !== id) {
+                $location.hash(id);
+            } else {
+                $anchorScroll();
+            }
+        }
+
+        $scope.colClass = 'col-md-6';
+        $scope.fullDiff = true;
+        $scope.showOrig = true;
+
+        $scope.setMode = function(mode) {
+            if (mode == 'sbs') {
+                $scope.colClass = 'col-md-6';
+                $scope.fullDiff = true;
+                $scope.showOrig = true;
+            } else if (mode == 'min') {
+                $scope.colClass = 'col-md-12';
+                $scope.fullDiff = false;
+                $scope.showOrig = false;
+            } else if (mode == 'full') {
+                $scope.colClass = 'col-md-12';
+                $scope.fullDiff = true;
+                $scope.showOrig = false;
+            }
+        }
     };
 
     return {
